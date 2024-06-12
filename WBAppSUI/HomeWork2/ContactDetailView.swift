f91f97d7aacbc74e4f28646cb4707098b8851703
Vladislav Naumov <101308224+99prblms@users.noreply.github.com>
2024-06-12 19:15:36 +0500

    Сверстал экран ContactDetailView

--- WBAppSUI/HomeWork2/ContactDetailView.swift
+++ WBAppSUI/HomeWork2/ContactDetailView.swift
@@ -6,25 +6,56 @@
 //
 
 import SwiftUI
-//
-//struct ContactDetailView: View {
-//    
-//    let contact: Contact
-//    
-//    var body: some View {
-//        VStack {
-//            Text(contact.name)
-//                .font(.largeTitle)
-//            Text(contact.status)
-//                .font(.title)
-//                .foregroundColor(.gray)
-//            Spacer()
-//        }
-//        .padding()
-//        .navigationTitle("Detail View")
-//    }
-//}
-//
-//#Preview {
-//    ContactDetailView()
-//}
+
+struct ContactDetailView: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        
+        VStack() {
+            Image(.changeAvatar)
+                .resizable()
+                .frame(width: 200, height: 200)
+                .cornerRadius(250)
+                .offset(CGSize(width: 0, height: -140))
+            VStack() {
+                Text("Иван Иванов")
+                    .font(.system(size: 24))
+                    .multilineTextAlignment(.center)
+                    .frame(width: 400)
+                    .fontWeight(.bold)
+                    .offset(CGSize(width: 0, height: -100))
+
+                Text("+7999-999-99-99")
+                    .font(.system(size: 20))
+                    .multilineTextAlignment(.center)
+                    .frame(width: 300)
+                    .foregroundColor(.gray)
+                    .offset(CGSize(width: 0, height: -90))
+            }
+            HStack() {
+                Image(.twitterPic)
+                    .resizable()
+                    .frame(width: 72, height: 40)
+                Image(.instaPic)
+                    .resizable()
+                    .frame(width: 72, height: 40)
+                Image(.linkedInPic)
+                    .resizable()
+                    .frame(width: 72, height: 40)
+                Image(.faceBookPic)
+                    .resizable()
+                    .frame(width: 72, height: 40)
+            }
+            .offset(CGSize(width: 0, height: -50))
+
+        }
+
+    }
+}
+
+struct ContactDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactDetailView()
+    }
+}
